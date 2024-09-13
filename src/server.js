03fbdd326f32c24338516cdfdcffec9a36088a14
Alex Becci <alex.becci@bingopilar.corp>
2024-09-13 17:09:20 -0300

    commit example

--- src/server.js
+++ src/server.js
@@ -15,17 +15,15 @@ app.use(express.json())
 app.use(morgan('dev'))
 //routes
 app.use(ClientRoutes)
+
 //publics
-
 startServer()
-
 
 //probar la conexion y luego levantar el servidor
 async function startServer() {
     try {
         //probar la conexion a la base de datos
         await testConnection();
-
         //si la conexion es exitosa, iniciar el servidor
         app.listen(3000)
         console.log('SERVER ON PORT --->3000')

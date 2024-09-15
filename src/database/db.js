3307bf662d728af1538a912a7cb6801d4fcb05ef
alex <beccialex2002@gmail.com>
2024-09-15 18:25:42 +0200

    import credentials .env

--- src/database/db.js
+++ src/database/db.js
@@ -1,11 +1,14 @@
+require('dotenv').config();
 const mysql = require('mysql2/promise')
 
-//configuracion de la conexion a la base de datos
+//configuracion de la conexion a la base de 
+console.log('DB_USER:', process.env.DB_USER);
+
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
     /* waitForConnections:true, */
 })
 
